eef8cc6db136cf9dd84147c4b3474c3b97760e65
Shaun Garwood <shaun.garwood@gmail.com>
2019-12-01 21:41:17 -0700

    run jekyll in draft mode

--- bin/start.sh
+++ bin/start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -t --rm --name ghp -v "$PWD":/usr/src/app -p "4000:4000" starefossen/github-pages
+docker run -t --rm --name ghp -v "$PWD":/usr/src/app -p "4000:4000" starefossen/github-pages jekyll serve -d /_site --watch --force_polling -H 0.0.0.0 -P 4000 --drafts
