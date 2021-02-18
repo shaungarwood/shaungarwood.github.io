f2404b60bb219cc8d1d6f8052a67a0ee3fa6eac4
Shaun Garwood <shaun.garwood@gmail.com>
2021-02-18 08:28:15 -0700

    bootstrap switch to main branch

--- bs.sh
+++ bs.sh
@@ -83,7 +83,7 @@ fi
 
 # if this script is standalone, will still need to checkout repo
 if [[ ! -d ~/my_bootstraps ]]; then
-    git clone --depth 1 --branch master https://github.com/shaungarwood/my_bootstraps.git ~/my_bootstraps
+    git clone --depth 1 --branch main https://github.com/shaungarwood/my_bootstraps.git ~/my_bootstraps
 else
     git pull ~/my_bootstraps/
 fi
